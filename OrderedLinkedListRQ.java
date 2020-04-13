c1fbff7d428441f800c5ee2e6ef348095479045d
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2020-04-13 14:59:28 +1000

    Add files via upload

--- OrderedLinkedListRQ.java
+++ OrderedLinkedListRQ.java
@@ -13,9 +13,6 @@ import java.util.Arrays;
 public class OrderedLinkedListRQ implements Runqueue {
 	
 	private int size;
-	//private String procList[];
-	//private int front[], back[];
-	//private int procVT[];
 	private Proc procArr[];
 	private final int INI_SIZE = 1;
 	
@@ -23,8 +20,7 @@ public class OrderedLinkedListRQ implements Runqueue {
     public OrderedLinkedListRQ() {
     	size = 2;
         procArr = new Proc[size];
-    	//front = new int[size];
-    	//back = new int[size];
+    	
     }  // end of OrderedLinkedList()
 
 
@@ -33,7 +29,15 @@ public class OrderedLinkedListRQ implements Runqueue {
         
     	//Assign the proc and its vt
     	this.procArr[this.size-this.INI_SIZE] = new Proc(procLabel,vt);
-		
+		/*
+		 * assgin the new proc to the back of the array
+		 * loop through the array to find suitable front and back node
+		 * set the front link to the front node
+		 * set the back link to the back node
+		 * set the back link of the front node to the current node
+		 * set the front link of the back node to the current node
+		 * expand the size of the array
+		 */
 		
 		int checkingPos = 1;
 		while(checkingPos != 0)
@@ -126,7 +130,11 @@ public class OrderedLinkedListRQ implements Runqueue {
 
     @Override
     public String dequeue() {
-        
+    	/*
+    	 * Removing the proc that have no front node
+    	 * Set the front link of the back node of the removed node as null (front of the queue)
+		 * return the removed node
+    	 */
     	Proc dequeue = null;
     	for(int x = 1; x < this.size; x++)
     	{
@@ -139,8 +147,8 @@ public class OrderedLinkedListRQ implements Runqueue {
     			
     		}
     	}
- 
-        return dequeue.label; // placeholder, modify this
+    	
+        return dequeue.label; 
     } // end of dequeue()
 
 
@@ -148,6 +156,11 @@ public class OrderedLinkedListRQ implements Runqueue {
     public boolean findProcess(String procLabel) {
     	boolean existed = false;
     
+    	/*
+    	 * loop through the array to find the procLabel
+    	 * return true if found
+    	 */
+    	
         for(Proc x : this.procArr)
         {
         	if(x != null && x.label.equals(procLabel))
@@ -163,6 +176,13 @@ public class OrderedLinkedListRQ implements Runqueue {
     public boolean removeProcess(String procLabel) {
     	boolean removed = false;
         
+    	/*
+    	 * loop through the array to find the procLabel
+    	 * set the back link of the front node of the removed node as the back link 
+    	 * set the front link of the back node the removed node as the front link
+    	 * return true if the node existed and removed
+    	 */
+    	
         for(int x = 1; x< this.size;x++)
         {
         	if(this.procArr[x] != null && this.procArr[x].label.equals(procLabel))
@@ -190,6 +210,12 @@ public class OrderedLinkedListRQ implements Runqueue {
     @Override
     public int precedingProcessTime(String procLabel) {
         
+    	/*
+    	 * loop through the array to find suitable proc
+    	 * loop through the linked list to calculate the vt of all front node
+    	 * return the total vt calculated
+    	 */
+    	
     	int TotalPT = -1;
     	for (int x = 1; x < this.size; x++)
     	{
@@ -212,7 +238,11 @@ public class OrderedLinkedListRQ implements Runqueue {
 
     @Override
     public int succeedingProcessTime(String procLabel) {
-    	
+    	/*
+    	 * loop through the array to find suitable proc
+    	 * loop through the linked list to calculate the vt of all back node
+    	 * return the total vt calculated
+    	 */
     	int TotalPT = -1;
     	
     	for (int x = 1; x < this.size; x++)
@@ -221,10 +251,9 @@ public class OrderedLinkedListRQ implements Runqueue {
     		{
     			TotalPT++;
     			int checkingPos = this.procArr[x].neighbor1;
-    			System.out.println(this.procArr[x].label);
+    		
     			while(checkingPos !=0)
-    			{System.out.println(this.procArr[checkingPos].label);
-    				TotalPT += this.procArr[checkingPos].vt;
+    			{TotalPT += this.procArr[checkingPos].vt;
     				checkingPos = this.procArr[checkingPos].neighbor1;
     			}
     			break;
@@ -237,7 +266,11 @@ public class OrderedLinkedListRQ implements Runqueue {
 
     @Override
     public void printAllProcesses(PrintWriter os) {
-     
+    	/*
+    	 * loop through the array to find the front node
+    	 * loop through the linked list to append the back node to the String
+    	 * print the String of the list
+    	 */
     	String AllProc = "";
     	for (int x = 1; x < this.size; x++)
     	{
